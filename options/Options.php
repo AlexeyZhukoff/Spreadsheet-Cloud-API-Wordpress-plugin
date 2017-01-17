524fb62e8bed106d2060d3db33a158b5359f5f9a
Alexey Zhukov <alexey.zhukov@devexpress.com>
2017-01-17 17:01:52 +0300

    Options form extract

--- options/Options.php
+++ options/Options.php
@@ -26,34 +26,12 @@ function mt_options_page() {
             show_header_message('File Uploaded.');
             update_option( 'userFilesList', SpreadsheetCloudAPIActions::GetFileList());
     }
-
-    echo '<div class="wrap">';
-    echo "<h2>" . __( 'Spreadsheet Cloud API Plugin Options', 'mt_trans_domain' ) . "</h2>";
-    ?>
-
-<form name="form1" method="post" action="<?= str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
-<input type="hidden" name="<?= $hidden_field_name; ?>" value="Y">
-
-<p><?php _e("API Key:", 'API_Key' ); ?> 
-<input type="text" name="<?= $data_field_name; ?>" value="<?= $opt_val; ?>" size="50">
-</p><hr />
-
-<p class="submit">
-<input type="submit" name="Submit" value="<?php _e('Update Options', 'mt_trans_domain' ) ?>" />
-</p>
-
-
-</form>
-
-<form class="user_upload_file none" enctype="multipart/form-data" method="post" action="<?= str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
-<?php wp_nonce_field('my_file_upload', 'fileup_nonce'); ?>
-<input class="file_input_text" name="my_file_upload" type="file" />
-<input class="btn" type="submit" value="Upload" />
-</form>
-
-</div>
-
-<?php
+    $optionsheader = __( 'Spreadsheet Cloud API Plugin Options', 'mt_trans_domain' );
+    $optionsaction = str_replace( '%7E', '~', $_SERVER['REQUEST_URI']);
+    $optionsapikey = __("API Key:", 'API_Key' );
+    $optionsupdate = __('Update Options', 'mt_trans_domain' );
+    $optionsnoncefield = wp_nonce_field('my_file_upload', 'fileup_nonce');
+    include (SPREADSHEEETCLOUDAPI__PLUGIN_DIR.'\options\Options.html');
 }
 function show_header_message($message){
     echo '<div class="updated"><p><strong>';
